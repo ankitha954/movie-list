2b4fc07b587b53fad22a948286f3206f5f87ba6e
mallela ankitha <mallelaankitha954@gmail.comm>
2022-10-22 14:48:49 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Movie from './components/Movie';
 
 const FeaturedApi="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const ImagesApi="https://image.tmdb.org/t/p/w1280";
  const SearchApi="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
   
